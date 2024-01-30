dfef123ec1f7e04a3afee76bac6b33eb1c71b954
Jhalddry <jhalddryg@gmail.com>
2024-01-30 04:29:53 -0400

    minor changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,22 +3,23 @@ import { useAuth } from "../context/AuthContext";
 
 function Navbar() {
   const { isAuthenticated, logout, user } = useAuth();
+  const capitalizedUsername = user?.username?.charAt(0).toUpperCase() + user?.username?.slice(1);
 
   return (
     <nav className="bg-zinc-700 my-3 flex justify-between py-5 px-10 rounded-lg">
       <Link to={
         isAuthenticated ? "/task" : "/"
       }>
-        <h1 className="text-2xl font-bold">Task Manager</h1>
+        <h1 className="text-2xl font-bold">Practice Project</h1>
       </Link>
       <ul className="flex gap-x-2">
         {isAuthenticated ? (
           <>
-            <li className="text-lg px-4 font-bold"> Welcome {user.username}!</li>
+            <li className="text-lg px-4 font-bold"> Welcome {capitalizedUsername}!</li>
 
             <li>
               <Link
-                to="/"
+                to="/login"
                 className="bg-red-400 px-4 py-1 rounded-sm"
                 onClick={() => {
                   logout();
