47e274ab416ffb541f12c700813a2367b5ac0b05
Jhalddry <jhalddryg@gmail.com>
2024-05-01 02:08:03 -0400

    tailwind added

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,8 @@
 export const HomePage = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-
-      <code>Page on progress...</code>
+    <div className="flex flex-col items-center">
+      <h1 className="text-4xl font-bold">Home Page</h1>
+      <h2 className="font-semibold">Page on Building...</h2>
     </div>
-  )
-}
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./client/src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
